aea38321da7dd9888382b5de3190a493ca61dff3
Jared Fraser <dev@jsf.io>
2014-07-17 11:36:33 +0100

    Added test script for CI

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+set -e
+
+export PATH=./bin:$PATH
+export RAILS_ENV=test
+export BUNDLE_WITHOUT=development
+
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+BUNDLE_JOBS=$((CORES-1))
+
+CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
+CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
+
+bundle install --jobs $BUNDLE_JOBS
+
+rake spec
+rake cucumber
