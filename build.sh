f97a09fd346a92c909a55274416694ef204189a5
Jared Fraser <dev@jsf.io>
2014-07-22 16:20:45 +0100

    Added build script
    Matches frontend

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+set -e
+
+unset RUBYOPT
+export BUNDLE_WITHOUT="test:development"
+
+version_number=${GO_PIPELINE_COUNTER-0}
+revision=`git rev-parse HEAD`
+build_date=`date +'%Y-%m-%d %H:%M %z'`
+
+cat > public/version <<EOT
+{
+  "version":"$version_number",
+  "buildDate":"$build_date",
+  "gitRevision":"$revision"
+}
+EOT
+
+echo "Cleaning temporary files"
+echo "----"
+rm -rf public/assets vendor/cache coverage log/* tmp/*
+
+echo "Running Bundle package"
+echo "----"
+bundle package --all
+
+echo "Running Bower cache clean"
+echo "----"
+bower cache clean
+
+echo "Running Bower install"
+echo "----"
+bower install --production --config.interactive=false
+
+echo "Precompiling assets"
+echo "----"
+RAILS_ENV=production RAILS_GROUPS=assets rake assets:precompile
+
+echo "Creating RPM"
+echo "----"
+cd ..
+/usr/local/rpm_builder/create-rails-rpm $artifact_name $artifact_name $version
+
+#prune packaged gems
+rm -rf vendor/cache
