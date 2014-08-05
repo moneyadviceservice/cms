1f3aa77aefbcef7743a13c3d6853d603b0ddec46
Jared Fraser <dev@jsf.io>
2014-08-05 12:30:11 +0100

    Updated root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/version', to: 'versions#show'
 
+  root to: redirect('/admin')
+
   comfy_route :cms_admin, :path => '/admin'
 
   # Make sure this routeset is defined last
