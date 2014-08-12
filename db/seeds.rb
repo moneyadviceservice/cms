726b275976cb92ec0bd2a5b334d17fa8f207abc0
Ruben Martin <Ruben.Martin@moneyadviceservice.org.uk>
2014-08-12 13:12:01 +0100

    Add db/seeds.rb to seed categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = YAML::load_file("#{Rails.root}/config/categories.yml")
+
+site = Comfy::Cms::Site.create(
+  label: "Money Advice Service",
+  identifier: "money-advice-service",
+  hostname: "cms.dev",
+  path: "cms",
+  locale: "en",
+  is_mirrored: false) if Rails.env == "development"
+
+categories.each do |parent, children|
+  labels = children.nil? ? [parent] : children
+  labels.each { |label| Comfy::Cms::Category.create(site_id: site.id, label: label, categorized_type: "Comfy::Cms::Page") }
+end
