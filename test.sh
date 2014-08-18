00df4e7f4e33db23b586e903f9dcc5936747a37e
Jared Fraser <dev@jsf.io>
2014-08-18 11:34:54 +0100

    Run bundle before rake tasts in test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,10 @@ export PATH=./bin:$PATH
 export RAILS_ENV=test
 export BUNDLE_WITHOUT=development
 
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+BUNDLE_JOBS=$((CORES-1))
+
+bundle install --jobs $BUNDLE_JOBS
 
 if [ -n "$GO_PIPELINE_NAME" ]; then
   cp config/database{-ci,}.yml
@@ -13,13 +17,8 @@ if [ -n "$GO_PIPELINE_NAME" ]; then
   rake db:drop db:create db:schema:load db:migrate
 fi
 
-CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-BUNDLE_JOBS=$((CORES-1))
-
 CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
 CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
-
-bundle install --jobs $BUNDLE_JOBS
 
 rake spec
 rake cucumber
