67c7e8adc7a7f5ada5f91d403d17ec7831db43e7
Jared Fraser <dev@jsf.io>
2014-08-20 15:43:12 +0100

    bundle exec bowndler

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 BUNDLE_JOBS=$((CORES-1))
 
 bundle install --jobs $BUNDLE_JOBS
-bowndler install
+bundle exec bowndler install
 
 if [ -n "$GO_PIPELINE_NAME" ]; then
   cp config/database{-ci,}.yml
