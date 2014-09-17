c0780951ba43eebd5d37f70cd7178ba181231e51
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-09-17 15:52:24 +0100

    Enable the auto resize textarea plugin

--- app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
@@ -5,9 +5,11 @@
 window.CMS.wysiwyg = function() {
   'use strict';
   require([
-    'mas-editor'
+    'mas-editor',
+    'editor-plugin-auto-resize-textarea'
   ], function (
-    masEditor
+    masEditor,
+    editorPluginAutoResizeTextarea
   ) {
     var markdownEditorContentNode = document.querySelector('.js-markdown-editor-content');
 
@@ -25,6 +27,8 @@ window.CMS.wysiwyg = function() {
       markdownEditorContentNode: markdownEditorContentNode,
       switchModeButtonNodes: document.querySelectorAll('.js-switch-mode')
     });
+
+    masEditor.editor.use(editorPluginAutoResizeTextarea(markdownEditorContentNode));
 
     var focusOnTitle = function() {
       var el = document.querySelector('input#page_label');
