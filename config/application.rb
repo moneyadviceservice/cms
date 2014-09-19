8d7e7bfb79a5487df8a26fe226a22e5408c28432
Jared Fraser <dev@jsf.io>
2014-09-19 10:41:24 +0100

    Added basic.css to precompile list

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Cms
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/lib)
+
+    config.assets.precompile += %w(comfortable_mexican_sofa/admin/basic.css)
   end
 end
