c401b549a2a3b803e40de8f889964e77c42778dc
Jared Fraser <dev@jsf.io>
2014-10-13 14:02:41 +0100

    Use StarWriter format to overcome word import issues on QA

--- app/models/word_document.rb
+++ app/models/word_document.rb
@@ -11,6 +11,6 @@ class WordDocument
   end
 
   def to_s
-    @_converted ||= parser.new(path, display: ':99').to_s
+    @_converted ||= parser.new(path, display: ':99', format: 'html:"HTML (StarWriter)"').to_s
   end
 end
