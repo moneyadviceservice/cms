67b9341886829ddc661ffdac511b79096027d8da
Lorenzo Tello <lorenzo.tello@moneyadviceservice.org.uk>
2014-10-13 13:42:15 +0100

    rename some methods and better js

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,14 +2,15 @@ module TagsHelper
 
   private
 
+
   # For every letter of the alphabet returns a link to tags startig by that letter.
-  def index_of_tags_in_use
+  def index_of_existing_tags
     (:a..:z).map {|letter| link_to_tags_starting_by(prefix: letter)}.join
   end
 
   # Renders the link to gather the list of tags that start with the give prefix:
   def link_to_tags_starting_by(prefix:)
-    link_to(prefix, starting_by_tags_path(prefix: prefix), remote: :true, data: {no_turbolink: :true})
+    link_to(prefix, starting_by_tags_path(prefix: prefix), remote: :true, data: {no_turbolink: :true, prefix: prefix}, class:'js-tags-starting-by-link')
   end
 
 end
