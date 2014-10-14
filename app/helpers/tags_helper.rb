4b3f34342c41ad31ecd0df993298b6ad3566a59d
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-10-14 16:58:49 +0100

    [WIP] Tags admin frontend styling

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -3,12 +3,12 @@ module TagsHelper
   private
 
 
-  # For every letter of the alphabet returns a link to tags startig by that letter.
+  # For every letter of the alphabet returns a link to tags starting with that letter.
   def index_of_existing_tags
     (:a..:z).map {|letter| link_to_tags_starting_by(prefix: letter)}.join
   end
 
-  # Renders the link to gather the list of tags that start with the give prefix:
+  # Renders the link to gather the list of tags that start with the given prefix:
   def link_to_tags_starting_by(prefix:)
     link_to(prefix, starting_by_tags_path(prefix: prefix), remote: :true, data: {no_turbolink: :true, prefix: prefix}, class:'js-tags-starting-by-link')
   end
