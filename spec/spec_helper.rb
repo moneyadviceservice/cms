830a64bf513166e04ec9218d43c3fa8658c1273b
Lorenzo Tello <lorenzo.tello@moneyadviceservice.org.uk>
2014-10-15 00:23:49 +0100

    add site_prism configuration. May help in the future

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require_relative 'support/cms'
 require 'mas/development_dependencies/rspec/spec_helper'
+
+# SitePrism.configure do |config|
+#   config.use_implicit_waits = true
+# end
