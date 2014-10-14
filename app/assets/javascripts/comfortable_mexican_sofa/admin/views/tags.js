9cfa044217e53e0c11819e36f251bbd4361326f0
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-10-14 13:55:51 +0100

    Taggle - Remove tabbing to close buttons, add active and tag classes
    Looking to wrap the Taggle script in a wrapper

--- app/assets/javascripts/comfortable_mexican_sofa/admin/views/tags.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/views/tags.js
@@ -5,6 +5,8 @@
 require(['taggle'], function(Taggle) {
   'use strict';
 
+  var taggle;
+
   // set 'active' class to selected letter tags link when clicking.
   $('.js-tags-starting-by-link').click(function() {
     $('.js-tags-existing .tag').removeClass('active');
@@ -13,7 +15,7 @@ require(['taggle'], function(Taggle) {
 
   // update the list of tags starting by <prefix> if needed.
   function maybeUpdateDisplayedTagList(prefix) {
-    if (displayedTagsPrefix() == prefix) {
+    if (displayedTagsPrefix() === prefix) {
       displayTagList(prefix);
     }
   }
@@ -32,12 +34,16 @@ require(['taggle'], function(Taggle) {
   }
 
   // inits the tag box
-  new Taggle(document.querySelector('.js-tags-display'), {
+  taggle = new Taggle(document.querySelector('.js-tags-display'), {
     placeholder: '',
+    containerFocusClass: 'is-active',
+    additionalTagClasses: 'tag',
     onTagAdd: function(event, tag) {
+      var tagListNodes = taggle.getTags().elements;
       $('.js-tags-add-value').val(tag);
       $('.js-tags-add-submit').click();
       maybeUpdateDisplayedTagList(tag[0]);
+      tagListNodes[tagListNodes.length - 1].querySelector('.close').setAttribute('tabIndex', -1);
     },
     onTagRemove: function(event, tag) {
       $('.js-tags-delete-value').val(tag);
