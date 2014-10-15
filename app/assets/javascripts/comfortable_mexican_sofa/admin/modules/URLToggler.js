1e4c7b701b0930bcd19e934fa3a9a9526e176dbf
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-10-15 15:48:19 +0100

    Convert the language switch code into a generic Dough component called URLSwitcher

---
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/modules/URLToggler.js
@@ -0,0 +1,34 @@
+define([
+  'jquery',
+  'DoughBaseComponent'
+], function (
+  $,
+  DoughBaseComponent
+) {
+  'use strict';
+
+  var URLTogglerProto,
+      defaultConfig = {
+        uiEvents: {
+          'change ': '_handleChange'
+        }
+      };
+
+  function URLToggler($el, config) {
+    URLToggler.baseConstructor.call(this, $el, config, defaultConfig);
+  };
+
+  DoughBaseComponent.extend(URLToggler);
+
+  URLTogglerProto = URLToggler.prototype;
+
+  URLTogglerProto.init = function(initialised) {
+    this._initialisedSuccess(initialised);
+  };
+
+  URLTogglerProto._handleChange = function() {
+    window.location = this.$el.find(':checked').data()['value'];
+  };
+
+  return URLToggler;
+});
