618cd558c390b30371e142506232aabfcd282ada
Lorenzo Tello <lorenzo.tello@moneyadviceservice.org.uk>
2014-10-16 17:03:29 +0100

    remove unused site_prism config block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,3 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require_relative 'support/cms'
 require 'mas/development_dependencies/rspec/spec_helper'
-
-# SitePrism.configure do |config|
-#   config.use_implicit_waits = true
-# end
