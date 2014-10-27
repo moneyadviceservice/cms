40fa0725c551e075c6c216d510a6814c326f3957
Lorenzo Tello <lorenzo.tello@moneyadviceservice.org.uk>
2014-10-27 19:06:29 +0000

    add page_name element to edit_page and set right css class to tags_chosen element

--- features/support/ui/pages/new.rb
+++ features/support/ui/pages/new.rb
@@ -13,7 +13,7 @@ module UI::Pages
     element :page_name, '#page_label'
     element :tags_input_box, '.t-tags-select .search-field input'
     elements :tags_choices,  '.t-tags-select option'
-    elements :tags_chosen,   '.t-tags-select .search_choice'
+    elements :tags_chosen,   '.t-tags-select .search-choice'
     element :meta_description, '#page_meta_description'
     element :meta_title, '#page_meta_title'
     element :save_button, '.t-save_unsaved'
