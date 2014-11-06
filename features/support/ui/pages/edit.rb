a1533eb6ec9e9044d8ef1e7e8f6d143ccf8554c2
Ruben Martin <Ruben.Martin@moneyadviceservice.org.uk>
2014-11-06 15:08:39 +0000

    Fix cucumber test failure

--- features/support/ui/pages/edit.rb
+++ features/support/ui/pages/edit.rb
@@ -23,6 +23,6 @@ module UI::Pages
     element :site_toggle_cy, "#site__mirrors input#edit-mode_cy"
     element :site_toggle_en, "#site__mirrors input#edit-mode_en"
     element :status, '#t-status'
-    element :regulated_checkbox, '#page_regulated'
+    element :regulated_checkbox, '#page_regulated_box'
   end
 end
