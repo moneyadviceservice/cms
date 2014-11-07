172dc7c0f13ca96f5d124a7ee8b7922490d95e9b
Lorenzo Tello <lorenzo.tello@moneyadviceservice.org.uk>
2014-11-07 15:18:52 +0000

    test suite refactored

--- features/step_definitions/files_admin_page_steps.rb
+++ features/step_definitions/files_admin_page_steps.rb
@@ -27,81 +27,91 @@ Then(/^(?:I should see |also )?a file (.+) section$/) do |section|
 end
 
 Then(/^I choose to sort files by name$/) do
-  @page.files_filters.form.sort_by.select('name (a to z)')
+
+  @page.files_filters.form.sort_by.select(sort_by_options[:name])
   wait_for_ajax_complete
 end
 
 Then(/^I choose to sort files by date$/) do
-  @page.files_filters.form.sort_by.select('date added (lattest first)')
+  @page.files_filters.form.sort_by.select(sort_by_options[:date])
   wait_for_ajax_complete
 end
 
 Then(/^(?:The files|They) get (?:sorted|ordered) by name$/) do
-  expect(@page.files_filters.form).to have_select('order', :selected => 'name (a to z)')
-  expect(@page.files_listing.files.map(&:label).map(&:text).join(', ')).to eq('afile_10, afile_8, afile_9')
+
+  expect(@page.files_filters.form).to have_select('order', :selected => sort_by_options[:name])
+  expect(@page.files_listing.files.map(&:label).map(&:text)).to eq(sample_filenames.sort)
 end
 
 Then(/^(?:The files|They) get (?:sorted|ordered) by date \(lattest first\)$/) do
-  expect(@page.files_filters.form).to have_select('order', :selected => 'date added (lattest first)')
-  expect(@page.files_listing.files.map(&:label).map(&:text).join(', ')).to eq('afile_8, afile_9, afile_10')
+
+  expect(@page.files_filters.form).to have_select('order', :selected => sort_by_options[:date])
+  expect(@page.files_listing.files.map(&:label).map(&:text)).to eq(sample_filenames.reverse)
 end
 
 Then(/^I choose to filter files by (.+) type$/) do |type|
+
   @page.files_filters.form.type.select(type)
   wait_for_ajax_complete
 end
 
 Then(/^Only (.+) files are shown$/) do |type|
+
   expect(@page.files_filters.form).to have_select('type', :selected => type)
-  expect(@page.files_listing.files.map(&:label).map(&:text)).to contain_exactly(*send("#{type}_filenames"))
+  expect(@page.files_listing.files.map(&:label).map(&:text)).to contain_exactly(*filenames_of(type: type))
 end
 
 Then(/^Only (.+) files sorted by date are shown$/) do |type|
-  expect(@page.files_filters.form).to have_select('order', :selected => 'date added (lattest first)')
+
+  expect(@page.files_filters.form).to have_select('order', :selected => sort_by_options[:date])
   expect(@page.files_filters.form).to have_select('type',  :selected => type)
-  expect(@page.files_listing.files.map(&:label).map(&:text)).to eq(send("#{type}_filenames").reverse)
+  expect(@page.files_listing.files.map(&:label).map(&:text)).to eq(filenames_of(type: type).reverse)
 end
 
 Then(/^Only (.+) files sorted by name are shown$/) do |type|
-  expect(@page.files_filters.form).to have_select('order', :selected => 'name (a to z)')
+
+  expect(@page.files_filters.form).to have_select('order', :selected => sort_by_options[:name])
   expect(@page.files_filters.form).to have_select('type',  :selected => type)
-  expect(@page.files_listing.files.map(&:label).map(&:text)).to eq(send("#{type}_filenames"))
+  expect(@page.files_listing.files.map(&:label).map(&:text)).to eq(filenames_of(type: type))
 end
 
 When(/^I add a new "(.*?)" file$/) do |type|
+
   step('I visit the new file page')
   add_file(type: type)
 end
 
 def add_some_files
-  add_file(type: :jpeg)
-  add_file(type: :jpeg)
-  add_file(type: :pdf)
+  sample_filenames.each { |filename| add_file(filename) }
 end
 
-def doc_filenames; %w()                 end
-def jpg_filenames; %w(afile_10 afile_9) end
-def pdf_filenames; %w(afile_8)          end
-def xls_filenames; %w()                 end
-
 # Uploads a new file into the system.
-def add_file(type:)
-  label, path, desc = new_test_file_attrs(type: type)
+def add_file(filename)
   step('I visit the new file page')
-  fill_in("file_label", with: label)
-  attach_file('file_file', path)
-  fill_in('file_description', with: desc)
+  fill_in("file_label", with: filename)
+  attach_file('file_file', filename)
+  fill_in('file_description', with: sample_file_description(filename))
   click_link_or_button('Upload File')
   wait_for_ajax_complete
 end
 
-# The label, path and desc of a new sample file
-def new_test_file_attrs(type:)
-  root  = 'afile'
-  index = 10 - Comfy::Cms::File.count
-  label = [root, index].join('_')
-  name  = [label, type].join('.')
-  desc  = ['this is the description for file', name].join(' ')
-  path  = File.join(Rails.root, 'features', 'support', name)
-  [label, path, desc]
+def file_type_options; %w(doc jpg pdf xls) end
+def sort_by_options; { name: 'name (a to z)', date: 'date added (lattest first)' } end
+def sample_file_description(filename); ['this is the description for file:', filename].join(' ') end
+def sample_files_path; File.join(Rails.root, 'features', 'support', 'files', '*') end
+def sample_filenames; @filenames ||= Dir.glob(sample_files_path) end
+def sample_filenames_string; sample_filenames.join(', ') end
+
+# def doc_filenames
+# def pdf_filenames
+# ...
+file_type_options.each do |type|
+  define_method("#{type}_filenames") do
+    sample_filenames.find_all { |filename| filename.end_with?(type)}
+  end
 end
+
+def filenames_of(type:)
+  send("#{type}_filenames")
+end
+
