4c241b25f8fe43c6026d498b9aa18d46b2eb0fa3
Lorenzo Tello <lorenzo.tello@moneyadviceservice.org.uk>
2014-11-13 11:10:06 +0000

    add helper to return css classify files

--- app/helpers/files_helper.rb
+++ app/helpers/files_helper.rb
@@ -1,5 +1,12 @@
 module FilesHelper
+
   private
+
+  FA_FILE_TYPES = { 'doc'  => "fa-file-word-o",
+                    'docx' => "fa-file-word-o",
+                    'xls'  => "fa-file-excel-o",
+                    'xlsx' => "fa-file-excel-o",
+                    'pdf'  => "fa-file-pdf-o" }
 
   # The url to a icon representing the file based in its extension.
   def url_of_file_icon(file)
@@ -8,11 +15,15 @@ module FilesHelper
     file.is_image? ? file.file.url(:cms_medium) : image_path(icon_name)
   end
 
+  # The url to a icon representing the file based in its extension.
+  def icon_class_of(file)
+    extension = file.file.original_filename.split('.').last
+    "fa #{FA_FILE_TYPES[extension]}"
+  end
+
   def options_to_sort_files(selected_option:)
-    options = {
-      t('comfy.admin.cms.files.index.sort_by.name') => :label,
-      t('comfy.admin.cms.files.index.sort_by.date') => 'position DESC'
-    }
+    options = { t('comfy.admin.cms.files.index.sort_by.name') => :label,
+                t('comfy.admin.cms.files.index.sort_by.date') => 'position DESC' }
     options_for_select(options, selected_option)
   end
 
