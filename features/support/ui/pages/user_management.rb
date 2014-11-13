dde0c66c9f8453720442705860878a62134285c6
Douglas Roper <dougdroper@gmail.com>
2014-11-13 14:12:26 +0000

    Delete user feature

--- features/support/ui/pages/user_management.rb
+++ features/support/ui/pages/user_management.rb
@@ -5,8 +5,9 @@ module UI::Pages
     set_url '/admin/users'
     element :new_user, '.t-new-user'
     element :create_user, '.t-create-user'
-    element :user_name, "#comfy_cms_user_name"
-    element :user_email, "#comfy_cms_user_email"
-    element :user_password, "#comfy_cms_user_password"
+    elements :delete_user, '.t-delete'
+    element :user_name, '#comfy_cms_user_name'
+    element :user_email, '#comfy_cms_user_email'
+    element :user_password, '#comfy_cms_user_password'
   end
 end
