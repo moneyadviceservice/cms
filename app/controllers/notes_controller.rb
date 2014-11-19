8020596b97d088a83342cdbb1efa3cb49c44f8af
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-11-19 11:06:41 +0000

    Add notes controller create action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,13 @@
 class NotesController < Comfy::Admin::Cms::BaseController
+  def create
+    @page  = @site.pages.find(params[:page_id])
+    @note  = @page.revisions.create!(data: {
+      note: { to: params[:description] },
+      author: {
+        id: current_user.id,
+        name: current_user.name
+      }
+    })
+    render nothing: true
+  end
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe NotesController do
+  let(:current_user) { create(:user) }
+
+  before do
+    sign_in current_user
+  end
+
+  describe 'POST /notes' do
+    let(:page) { create(:page) }
+
+    before do
+      post :create, site_id: page.site_id, page_id: page.id, description: 'note-text'
+    end
+
+    it 'creates the page note' do
+      expect(assigns[:note]).to be_persisted
+      expect(assigns[:note].data).to eq(
+        note: {
+          to: 'note-text'
+        },
+        author: {
+          id:   current_user.id,
+          name: current_user.name
+        }
+      )
+    end
+  end
 end
