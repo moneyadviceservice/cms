2216afba9cbeeae967be76a789a3af0d3af35f5b
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-12-03 10:23:23 +0000

    Only search published pages in page link dialog

--- app/controllers/links/pages_controller.rb
+++ app/controllers/links/pages_controller.rb
@@ -1,6 +1,6 @@
 class Links::PagesController < ApplicationController
   def index
-    pages  = Comfy::Cms::Page.includes(:site)
+    pages  = Comfy::Cms::Page.includes(:site).published
     pages  = Comfy::Cms::Search.new(pages, params[:search]).results
 
     @pages = PageMirror.collect(pages)
