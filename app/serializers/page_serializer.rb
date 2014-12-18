450b58562fdc8cf8eb49e4fafaffb1f1ff30417f
jiggypete <jiggypete@gmail.com>
2014-12-18 15:34:55 +0000

    retrieving page views from google analytics and applying them to db articles

--- app/serializers/page_serializer.rb
+++ app/serializers/page_serializer.rb
@@ -3,7 +3,13 @@ require 'active_model_serializers'
 class PageSerializer < ActiveModel::Serializer
   attributes :label, :slug, :full_path,
              :meta_description, :meta_title, :category_names,
-             :layout_identifier
+             :layout_identifier, :related_content
 
   has_many :blocks, serializer: BlockSerializer
+
+  def related_content
+    {popular_links: [ {title: 'Sharky', path: '/cheese' },
+                      {title: 'George', path: '/wine' }
+                       ]}
+  end
 end
---
+++ lib/cms/update_page_views_task.rb
@@ -0,0 +1,13 @@
+require_relative '../google_analytics/api'
+
+class UpdatePageViewsTask
+
+  def self.run
+    google_analytics_results = GoogleAnalytics::API.fetch_article_page_views
+
+    Comfy::Cms::Page.all_english_articles.each do |article|
+      article.update_page_views google_analytics_results
+    end
+  end
+
+end
