0e05dcbff7c2ce25911f0463b4655cf296161d6f
Caden Lovelace <caden@herostrat.us>
2015-01-05 13:33:53 +0000

    Put begin/rescue around #update_page_views in rake task.
    Previously this conditional would log an error if an article hadn't been
    viewed in the current analytics period. That's not what we meant, so
    we'll just catch whatever exceptions we get.

--- lib/cms/update_page_views_task.rb
+++ lib/cms/update_page_views_task.rb
@@ -6,8 +6,10 @@ class UpdatePageViewsTask
     google_analytics_results = GoogleAnalytics::API.fetch_article_page_views
 
     Comfy::Cms::Page.all_english_articles.each do |article|
-      unless article.update_page_views google_analytics_results
-        log("Error updating article: #{article.id}")
+      begin
+        article.update_page_views(google_analytics_results)
+      rescue
+        log("Error updating article: #{article.id} (#{$ERROR_INFO.message})")
       end
     end
     log('Completed UpdatePageViewsTask')
