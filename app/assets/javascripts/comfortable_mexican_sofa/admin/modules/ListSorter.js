4731e3cb8d6a9af61f3b7bb72103d025ccbe1820
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2015-01-16 14:56:41 +0000

    Add ListSorter component

---
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/modules/ListSorter.js
@@ -0,0 +1,58 @@
+define([
+  'jquery',
+  'DoughBaseComponent',
+  'html-sortable'
+], function (
+  $,
+  DoughBaseComponent
+) {
+  'use strict';
+
+  var ListSorterProto,
+      defaultConfig = {
+        selectors: {
+          listOrderfield: 'data-dough-listsorter-order-field',
+          itemId: 'data-dough-listsorter-item-id',
+          context: 'data-dough-listsorter-context'
+        }
+      };
+
+  function ListSorter($el, config) {
+    ListSorter.baseConstructor.call(this, $el, config, defaultConfig);
+  }
+
+  DoughBaseComponent.extend(ListSorter);
+
+  ListSorterProto = ListSorter.prototype;
+
+  ListSorterProto.init = function(initialised) {
+    this.$el.sortable().bind('sortupdate', $.proxy(this._handleListUpdate, this));
+    this._cacheComponentElements();
+    this._initialisedSuccess(initialised);
+  };
+
+  ListSorterProto._cacheComponentElements = function() {
+    this.$listOrderFields = $('[' + this.config.selectors.listOrderfield + '][' + this.config.selectors.context + '=' + this.$el.attr(this.config.selectors.context) + ']');
+  };
+
+  ListSorterProto._handleListUpdate = function() {
+    this.setOrderFieldValue('[' + this.getListOrderArray() + ']');
+  };
+
+  ListSorterProto.getListOrderArray = function() {
+    var arr = [],
+        _this = this;
+
+    $.each(this.$el.find('[' + this.config.selectors.itemId + ']'), function() {
+      return arr.push($(this).attr(_this.config.selectors.itemId));
+    });
+
+    return arr;
+  };
+
+  ListSorterProto.setOrderFieldValue = function(val) {
+    this.$listOrderFields.val(val);
+  };
+
+  return ListSorter;
+});
