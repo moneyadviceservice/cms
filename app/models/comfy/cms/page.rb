f22dbc979ed30becd7b047b25addc56704f50d2c
Douglas Roper <dougdroper@gmail.com>
2015-01-16 16:49:38 +0000

    Fix categorization join

--- app/models/comfy/cms/page.rb
+++ app/models/comfy/cms/page.rb
@@ -36,7 +36,7 @@ class Comfy::Cms::Page < ActiveRecord::Base
     joins(
       'INNER JOIN comfy_cms_categorizations
        ON comfy_cms_categorizations.categorized_id = comfy_cms_pages.id'
-    ).where('comfy_cms_categorization.category_id = ?', category_id)
+    ).where('comfy_cms_categorizations.category_id = ?', category_id)
   end
 
   def update_page_views(analytics)
