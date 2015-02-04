dc94f0765414071e4c4e57f79a7641e7ed6014d6
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2015-02-04 11:56:39 +0000

    Add URLFormatter component

---
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/modules/URLGenerator.js
@@ -0,0 +1,115 @@
+define([
+  'jquery',
+  'DoughBaseComponent'
+], function (
+  $,
+  DoughBaseComponent
+) {
+  'use strict';
+
+  var URLFormatterProto,
+      defaultConfig = {
+        selectors: {
+          locationSelect: '[data-dough-urlformatter-location]',
+          slugInput: '[data-dough-urlformatter-slug]',
+          input: '[data-dough-urlformatter-input]',
+          urlDisplay: '[data-dough-urlformatter-url-display]'
+        }
+      };
+
+  function URLFormatter($el, config) {
+    URLFormatter.baseConstructor.call(this, $el, config, defaultConfig);
+  }
+
+  DoughBaseComponent.extend(URLFormatter);
+
+  URLFormatterProto = URLFormatter.prototype;
+
+  URLFormatterProto.init = function(initialised) {
+    this.baseUrl = this.$el.attr('data-dough-urlformatter-base-url');
+    this._cacheComponentElements();
+    this._setupUIEvents();
+    this._initialisedSuccess(initialised);
+  };
+
+  URLFormatterProto._cacheComponentElements = function() {
+    this.$titleInput = this.$el.find(this.config.selectors.input);
+    this.$locationSelect = this.$el.find(this.config.selectors.locationSelect);
+    this.$slugInput = this.$el.find(this.config.selectors.slugInput);
+    this.$urlDisplays = this.$el.find(this.config.selectors.urlDisplay);
+  };
+
+  URLFormatterProto._setupUIEvents = function() {
+    this.$titleInput.on('input', $.proxy(this._handleTitleInput, this));
+    this.$locationSelect.on('change', $.proxy(this._handleUserInput, this));
+    this.$slugInput.on('input', $.proxy(this._handleSlugInput, this));
+  };
+
+  URLFormatterProto.buildUrl = function(baseUrl, data) {
+    return baseUrl.replace(/{([^{}]*)}/g,
+      function (a, b) {
+        return data[b];
+      }
+    );
+  };
+
+  URLFormatterProto.updateDisplay = function(userInput) {
+    this.$urlDisplays.text(userInput);
+  };
+
+  URLFormatterProto.getUserInput = function() {
+    return {
+      location: this.$locationSelect.val(),
+      slug: this.$slugInput.val()
+    };
+  };
+
+  URLFormatterProto._unbindInputEvent = function() {
+    this.$titleInput.off('input', this._handleTitleInput);
+  };
+
+  URLFormatterProto._handleTitleInput = function () {
+    this.slugifySlugInput(this.$titleInput.val());
+    this._handleUserInput();
+  };
+
+  URLFormatterProto._handleUserInput = function() {
+    this.updateDisplay(this.buildUrl(this.baseUrl, this.getUserInput()));
+  };
+
+  URLFormatterProto._handleSlugInput = function() {
+    this.slugifySlugInput(this.$slugInput.val());
+    this._handleUserInput();
+    this._unbindInputEvent();
+  };
+
+  URLFormatterProto.slugifySlugInput = function(str) {
+    this.$slugInput.val(this.slugify(str));
+  };
+
+  URLFormatterProto.slugify = function(str) {
+    var i,
+        from,
+        to,
+        charsToRemove,
+        charsToReplaceWithDelimiter;
+
+    str = str.replace(/^\s+|\s+$/g, '');
+
+    from = 'ÀÁÄÂÃÈÉËÊÌÍÏÎÒÓÖÔÕÙÚÜÛàáäâãèéëêìíïîòóöôõùúüûÑñÇç';
+    to = 'aaaaaeeeeiiiiooooouuuuaaaaaeeeeiiiiooooouuuunncc';
+
+    for(i = 0; i < [from.length - 1];i++) {
+      str = str.replace(new RegExp(from[i], 'g'), to[i]);
+    }
+
+    charsToReplaceWithDelimiter = new RegExp('[·/,:;_]', 'g');
+    charsToRemove = new RegExp('[^a-zA-Z0-9 -]', 'g');
+    str = str.replace(charsToReplaceWithDelimiter, '-');
+    str = str.replace(charsToRemove, '').replace(/\s+/g, '-').toLowerCase();
+
+    return str;
+  };
+
+  return URLFormatter;
+});
