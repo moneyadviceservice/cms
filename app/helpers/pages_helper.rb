7dfaf9456973e1e40f3231b64f5c32e84ac8b1a0
Tomas D'Stefano <tomas_stefano@successoft.com>
2015-02-06 13:00:47 +0000

    Add data attribute when changing the page type on page form

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,14 @@ module PagesHelper
   def dough_component(new_record, components = [])
     new_record ? { dough_component: components.join(' ') } : {}
   end
+
+  def page_type_options_for(site)
+    site.layouts.map do |layout|
+      [
+        layout.label,
+        layout.id,
+        { 'data-dough-urlformatter-page-type-value' => layout.identifier.pluralize }
+      ]
+    end
+  end
 end
