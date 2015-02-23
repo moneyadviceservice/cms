88255e5a90752170f094338893d124a3fbc61e82
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2015-02-23 11:49:13 +0000

    Abstract page_form_component() innards to components() method

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,6 @@
 module PagesHelper
   def page_form_component(page, default: [], optional: [])
-    components = if page.new_record?
-                   [default, optional].flatten
-                 else
-                   default
-                 end
-
-    { dough_component: components.join(' ') }
+    { dough_component: components(page, default, optional).join(' ') }
   end
 
   def page_type_options_for(site)
@@ -36,6 +30,14 @@ module PagesHelper
   def slug_tag(page_type_url, slug)
     content_tag(:span, data: { dough_urlformatter_url_display: true }) do
       "#{page_type_url}/#{slug}"
+    end
+  end
+
+  def components(page, default, optional)
+    if page.new_record?
+      [default, optional].flatten
+    else
+      default
     end
   end
 end
