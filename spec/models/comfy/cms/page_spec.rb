bf1a7a5141779709d74b85e96abc4897f7d0bbe7
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2015-03-25 15:28:37 +0000

    Fix double quotes

--- spec/models/comfy/cms/page_spec.rb
+++ spec/models/comfy/cms/page_spec.rb
@@ -265,7 +265,7 @@ RSpec.describe Comfy::Cms::Page do
   describe 'has ever been published' do
     let(:page) { create(:page) }
 
-    context "when there is a previous published event" do
+    context 'when there is a previous published event' do
       before { page.revisions.create!(data: {event: 'published'}) }
 
       it 'when it has been published' do
