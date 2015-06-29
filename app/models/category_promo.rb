1678fed2f01b064efd6922cc5046fc03a30fd2eb
Phil Lee <asmega@ph-lee.com>
2015-06-29 16:02:57 +0100

    add validations to category promo

--- app/models/category_promo.rb
+++ app/models/category_promo.rb
@@ -2,4 +2,8 @@ class CategoryPromo < ActiveRecord::Base
   PROMO_TYPES = %w{ blog recommended popular calculator tool }
 
   belongs_to :category, class: Comfy::Cms::Category
+
+  validates :title, presence: true
+  validates :promo_type, inclusion: PROMO_TYPES
+  validates :locale, inclusion: %w{ en cy }
 end
--- spec/models/category_promo_spec.rb
+++ spec/models/category_promo_spec.rb
@@ -1,3 +1,11 @@
 describe CategoryPromo do
   it { is_expected.to belong_to(:category) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_inclusion_of(:promo_type).
+        in_array(CategoryPromo::PROMO_TYPES) }
+    it { should validate_inclusion_of(:locale).
+        in_array(%w{ en cy }) }
+  end
 end
