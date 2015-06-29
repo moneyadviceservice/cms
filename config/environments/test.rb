a396684229e2781ba609add5502b5c321733aac4
Phil Lee <asmega@ph-lee.com>
2015-06-29 11:15:50 +0100

    Revert "add asset host config for test environment"
    This reverts commit c01f50641f53d405369876c0b2d0a879e1ee8d87.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.action_controller.asset_host = 'http://localhost:3000'
 end
