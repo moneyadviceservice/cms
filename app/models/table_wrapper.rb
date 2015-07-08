867ed4de4d44ddbb19a7b98946d75b0827e9dc65
Phil Lee <asmega@ph-lee.com>
2015-07-08 16:37:25 +0100

    post process html output to wrap table elements
    - Kramdown library isn't very extensisble so not done at that level
    - this is done to enable scrollable tables

---
+++ app/models/table_wrapper.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+
+class TableWrapper
+  attr_reader :source
+
+  def initialize(source)
+    @source = source
+  end
+
+  def call
+    doc = Nokogiri::HTML.fragment(source)
+    nodes = doc.css "table"
+    nodes.wrap('<div class="table-wrapper"></div>')
+    doc.to_s
+  end
+end
