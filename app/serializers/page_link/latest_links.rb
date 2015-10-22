f59dd9ecf0149b883ab8a40db20a3db06ccb1240
Phil Lee <asmega@ph-lee.com>
2015-10-22 14:19:57 +0100

    cache publify links for 5 minutes

--- app/serializers/page_link/latest_links.rb
+++ app/serializers/page_link/latest_links.rb
@@ -1,7 +1,7 @@
 module PageLink
   class LatestLinks < PageLink::Base
     def as_json
-      Rails.cache.fetch("publify_api_latest_links_3", expires_in: 1.minute) do
+      Rails.cache.fetch("publify_api_latest_links_3", expires_in: 5.minutes) do
         Publify::API.latest_links(3).map do |input_blog_post|
           {
             title: input_blog_post['title'],
