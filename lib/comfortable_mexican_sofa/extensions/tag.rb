75d43240cb19b14d40e0515e0ef2334d406df8a0
Phil Lee <asmega@ph-lee.com>
2015-11-25 14:51:00 +0000

    test monkey patch

--- lib/comfortable_mexican_sofa/extensions/tag.rb
+++ lib/comfortable_mexican_sofa/extensions/tag.rb
@@ -1,9 +1,13 @@
 # MONKEY PATCH
 
 module ComfortableMexicanSofa
-  module Tag
-    def is_cms_block?
-      %w(page field collection image).member?(self.class.to_s.demodulize.underscore.split(/_/).first)
+  module Extensions
+    module Tag
+      def is_cms_block?
+        %w(page field collection image).member?(self.class.to_s.demodulize.underscore.split(/_/).first)
+      end
     end
   end
 end
+
+ComfortableMexicanSofa::Tag::InstanceMethods.send :prepend, ComfortableMexicanSofa::Extensions::Tag
