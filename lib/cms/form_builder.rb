bf8a998a38f63d770ecb2608cdf037a253ec831f
Phil Lee <asmega@ph-lee.com>
2015-11-25 13:59:58 +0000

    remove comments

--- lib/cms/form_builder.rb
+++ lib/cms/form_builder.rb
@@ -9,9 +9,7 @@ class Cms::FormBuilder < ComfortableMexicanSofa::FormBuilder
 
   def page_image(tag, index)
     markup = '<div class="form-group form-image" data-dough-component="FormImage" data-dough-form-image-identifier="home-edit-field-' + index.to_s + '">' +
-    '<!-- start -->' + 
-    default_tag_field(tag, index, :text_field_tag, data: { 'dough-form-image-input' => '' }) + 
-    '<!-- end it -->' + 
+    default_tag_field(tag, index, :text_field_tag, data: { 'dough-form-image-input' => '' }) +
     '<div class="form-image__preview-wrapper"><div class="form-image__preview"></div></div>' +
     '<button class="button--action form-image__button" data-dough-component="Dialog" data-dough-dialog-context="insert-image" data-dough-dialog-identifier="home-edit-field-' + index.to_s + '" type="button"><span class="button__text">Select image</span></button>' +
     '<button class="button--action form-image__button--remove" type="button"><span class="button__text">Remove</span></button>' +
