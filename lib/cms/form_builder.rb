ab986d73d5322273f409c9f3ee60a75f740a22cd
Phil Lee <asmega@ph-lee.com>
2015-11-26 12:06:10 +0000

    refactor to use heredoc on form builder element

--- lib/cms/form_builder.rb
+++ lib/cms/form_builder.rb
@@ -8,12 +8,14 @@ class Cms::FormBuilder < ComfortableMexicanSofa::FormBuilder
   end
 
   def page_image(tag, index)
-    markup = '<div class="form-group form-image" data-dough-component="FormImage" data-dough-form-image-identifier="home-edit-field-' + index.to_s + '">' +
-    default_tag_field(tag, index, :text_field_tag, data: { 'dough-form-image-input' => '' }) +
-    '<div class="form-image__preview-wrapper"><div class="form-image__preview"></div></div>' +
-    '<button class="button--action form-image__button" data-dough-component="Dialog" data-dough-dialog-context="insert-image" data-dough-dialog-identifier="home-edit-field-' + index.to_s + '" type="button"><span class="button__text">Select image</span></button>' +
-    '<button class="button--action form-image__button--remove" type="button"><span class="button__text">Remove</span></button>' +
-    '</div>'
+    markup = <<-eos
+      <div class="form-group form-image" data-dough-component="FormImage" data-dough-form-image-identifier="home-edit-field-#{index.to_s}">
+      #{default_tag_field(tag, index, :text_field_tag, data: { 'dough-form-image-input' => '' })}
+      <div class="form-image__preview-wrapper"><div class="form-image__preview"></div></div>
+      <button class="button--action form-image__button" data-dough-component="Dialog" data-dough-dialog-context="insert-image" data-dough-dialog-identifier="home-edit-field-#{index.to_s}" type="button"><span class="button__text">Select image</span></button>
+      <button class="button--action form-image__button--remove" type="button"><span class="button__text">Remove</span></button>
+      </div>
+    eos
 
     markup.html_safe
   end
