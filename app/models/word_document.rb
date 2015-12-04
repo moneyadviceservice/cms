55efdea5323de44a89530db8aef81efb96a94f6d
Phil Lee <asmega@ph-lee.com>
2015-12-04 17:58:32 +0000

    remove display option form word doc upload

--- app/models/word_document.rb
+++ app/models/word_document.rb
@@ -9,6 +9,6 @@ class WordDocument
   end
 
   def to_s
-    @_converted ||= parser.new(path, display: ':99', format: 'html:"HTML (StarWriter)"').to_s
+    @_converted ||= parser.new(path, format: 'html:"HTML (StarWriter)"').to_s
   end
 end
