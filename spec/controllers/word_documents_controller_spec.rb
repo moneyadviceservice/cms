b593e064db7c75aaab046f096740b8f5380aba58
Phil Lee <asmega@ph-lee.com>
2015-12-07 15:53:19 +0000

    use pandoc not openoffice to parse word docs
    also makes uploading word docs partially usable compared to not usable
    at all
    
    unpend tests around word doc uploads
    
    remove word to markdown gem as no longer needed

--- spec/controllers/word_documents_controller_spec.rb
+++ spec/controllers/word_documents_controller_spec.rb
@@ -1,15 +1,22 @@
 RSpec.describe WordDocumentsController, type: :controller do
   include_context 'cms'
 
+  let(:current_user) { create(:user) }
+
+  before do
+    sign_in current_user
+  end
+
   describe 'create' do
     let(:filepath) { Rails.root.join('spec/fixtures/structured_doc.docx') }
     let(:content_type) { Rack::Mime.mime_type(File.extname(filepath)) }
     let(:document) { Rack::Test::UploadedFile.new(filepath, content_type) }
 
     it 'creates a new article with the word document contents' do
-      # post :create, word: { document: document }
+      post :create, word: { document: document }, format: 'js'
 
-      # expect(assigns(:page).blocks.first.content).to_not be_nil
+      expect(assigns(:doc)).to_not be_nil
+      expect(assigns(:doc).to_markdown).to include('# Heading One')
     end
   end
 end
