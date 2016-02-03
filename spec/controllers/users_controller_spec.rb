363c853c90a4fa0178d8248a72429ab6fee0f8c9
Kate Wilkinson <kate.wilkinson@hotmail.com>
2016-02-03 12:13:17 +0000

    Request to change "author" to "user"

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe UsersController, type: :controller  do
     let(:other_user) { create(:user) }
 
     context 'non admin' do
-      let(:role) { Comfy::Cms::User.roles[:author] }
+      let(:role) { Comfy::Cms::User.roles[:user] }
 
       context 'editing someone else\'s user' do
         before { get :edit, id: other_user.id }
