4712f3f1dc2b98e09a13b10b7c5a480edfb1dbd8
Kate Wilkinson <kate.wilkinson@hotmail.com>
2016-02-03 09:57:08 +0000

    add role attribute to users

--- spec/models/comfy/cms/user_spec.rb
+++ spec/models/comfy/cms/user_spec.rb
@@ -1,4 +1,20 @@
 describe Comfy::Cms::User do
+  describe 'validations' do
+    it { should validate_presence_of(:role) }
+  end
+
+  describe 'role' do
+    it 'allows the role to be set as a "user"' do
+      subject.role = 0
+      expect(subject.role).to eq('user')
+    end
+
+    it 'allows the role to be set as an admin' do
+      subject.role = 1
+      expect(subject.role).to eq('admin')
+    end
+  end
+
   describe '#email_local_part' do
     it 'returns the local part' do
       subject.email = 'john.doe@example.com'
