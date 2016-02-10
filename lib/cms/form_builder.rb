201f0721ab6f6dd34fe3181f02d4a05faf5810e8
Kate Wilkinson <kate.wilkinson@hotmail.com>
2016-02-10 16:09:05 +0000

    adjust formatting to appease rubocop

--- lib/cms/form_builder.rb
+++ lib/cms/form_builder.rb
@@ -9,11 +9,18 @@ class Cms::FormBuilder < ComfortableMexicanSofa::FormBuilder
 
   def page_image(tag, index)
     markup = <<-eos
-      <div class="form-group form-image" data-dough-component="FormImage" data-dough-form-image-identifier="home-edit-field-#{index.to_s}">
+      <div class="form-group form-image"
+           data-dough-component="FormImage"
+           data-dough-form-image-identifier="home-edit-field-#{index}">
       #{default_tag_field(tag, index, :text_field_tag, data: { 'dough-form-image-input' => '' })}
       <div class="form-image__preview-wrapper"><div class="form-image__preview"></div></div>
-      <button class="button--action form-image__button" data-dough-component="Dialog" data-dough-dialog-context="insert-image" data-dough-dialog-identifier="home-edit-field-#{index.to_s}" type="button"><span class="button__text">Select image</span></button>
-      <button class="button--action form-image__button--remove" type="button"><span class="button__text">Remove</span></button>
+      <button class="button--action form-image__button"
+              data-dough-component="Dialog"
+              data-dough-dialog-context="insert-image"
+              data-dough-dialog-identifier="home-edit-field-#{index}"
+              type="button"><span class="button__text">Select image</span></button>
+      <button class="button--action form-image__button--remove"
+              type="button"><span class="button__text">Remove</span></button>
       </div>
     eos
 
