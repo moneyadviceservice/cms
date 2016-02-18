73c24d2a422063af898e689af88f77eb60680e4a
Kate Wilkinson <kate.wilkinson@hotmail.com>
2016-02-18 14:49:44 +0000

    Ability to create contact_panels pages.

--- lib/cms/page_builder.rb
+++ lib/cms/page_builder.rb
@@ -20,5 +20,36 @@ module Cms
       page.save_unsaved!
       page.publish!
     end
+
+    def self.add_contact_panels_page!
+      layout       = english_site.layouts.find_by(identifier: 'contact_panels')
+      english_page = Comfy::Cms::Page.create!(slug: 'contact-panels', site: english_site, layout: layout)
+      welsh_page   = welsh_site.pages.find_by(slug: 'contact-panels')
+
+      create_page_blocks layout, [english_page, welsh_page]
+      publish_pages [english_page, welsh_page]
+    end
+
+    def self.english_site
+      Comfy::Cms::Site.find_by(label: 'en')
+    end
+
+    def self.welsh_site
+      Comfy::Cms::Site.find_by(label: 'cy')
+    end
+
+    def self.create_page_blocks(layout, pages)
+      content_areas = layout.content.scan(/{{\s*\w+\:\w+\:(\w+)/).map(&:first)
+      content_areas.each do |content_area|
+        pages.each { |page| page.blocks.create!(identifier: content_area, content: '') }
+      end
+    end
+
+    def self.publish_pages(pages)
+      pages.each do |page|
+        page.save_unsaved!
+        page.publish!
+      end
+    end
   end
 end
--- spec/lib/cms/page_builder_spec.rb
+++ spec/lib/cms/page_builder_spec.rb
@@ -1,13 +1,15 @@
 RSpec.describe Cms::PageBuilder do
-  let!(:site)   { create(:site) }
-  let!(:article_layout) { create(:layout, identifier: 'article', site: site) }
-  let!(:home_page_layout) { create(:layout, identifier: 'home_page', site: site) }
+  let!(:english_site)          { create(:site, is_mirrored: true) }
+  let!(:welsh_site)            { create(:site, :welsh, is_mirrored: true) }
+  let!(:article_layout)        { create(:layout, identifier: 'article', site: english_site) }
+  let!(:home_page_layout)      { create(:layout, identifier: 'home_page', site: english_site) }
+  let!(:contact_panels_layout) { create(:layout, identifier: 'contact_panels', site: english_site) }
 
   describe '.add_example_article!' do
     before { Cms::PageBuilder.add_example_article! }
 
     it 'adds a page' do
-      expect(Comfy::Cms::Page.count).to eq(1)
+      expect(english_site.pages.count).to eq(1)
     end
 
     it 'adds a page using the "article" layout' do
@@ -16,12 +18,12 @@ RSpec.describe Cms::PageBuilder do
   end
 
   describe '.add_home_page!' do
-    let(:page) { Comfy::Cms::Page.first }
-
     before { Cms::PageBuilder.add_home_page! }
 
+    let(:page) { english_site.pages.reload.first }
+
     it 'adds a page' do
-      expect(Comfy::Cms::Page.count).to eq(1)
+      expect(english_site.pages.count).to eq(1)
     end
 
     it 'adds a page using the "home_page" layout' do
@@ -29,7 +31,7 @@ RSpec.describe Cms::PageBuilder do
     end
 
     it 'adds the page to the english site' do
-      expect(page.site).to eq(site)
+      expect(page.site).to eq(english_site)
     end
 
     it 'sets the slug to be "the-money-advice-service"' do
@@ -42,6 +44,66 @@ RSpec.describe Cms::PageBuilder do
 
     it 'publishes the home_page' do
       expect(page).to be_published
+    end
+  end
+
+  describe '.add_contact_panels_page!' do
+    before { Cms::PageBuilder.add_contact_panels_page! }
+
+    context 'English site' do
+      let(:page) { english_site.pages.reload.first }
+
+      it 'adds a page' do
+        expect(english_site.pages.count).to eq(1)
+      end
+
+      it 'adds a page using the "contact_panels" layout' do
+        expect(page.layout).to eq(contact_panels_layout)
+      end
+
+      it 'adds the page to the english site' do
+        expect(page.site).to eq(english_site)
+      end
+
+      it 'sets the slug to be "contact-panels"' do
+        expect(page.slug).to eq('contact-panels')
+      end
+
+      it "generates blank 'blocks' for each of the layout's content areas" do
+        expect(page.blocks.count).to eq(3)
+      end
+
+      it 'publishes the home_page' do
+        expect(page).to be_published
+      end
+    end
+
+    context 'Welsh site' do
+      let(:page) { welsh_site.pages.reload.first }
+
+      it 'adds a page' do
+        expect(welsh_site.pages.count).to eq(1)
+      end
+
+      it 'adds a page using the "contact_panels" layout' do
+        expect(page.layout.identifier).to eq('contact_panels')
+      end
+
+      it 'adds the page to the welsh site' do
+        expect(page.site).to eq(welsh_site)
+      end
+
+      it 'sets the slug to be "contact-panels"' do
+        expect(page.slug).to eq('contact-panels')
+      end
+
+      it "generates blank 'blocks' for each of the layout's content areas" do
+        expect(page.blocks.count).to eq(3)
+      end
+
+      it 'publishes the home_page' do
+        expect(page).to be_published
+      end
     end
   end
 end
