67f326bb9bd68e56ba7317d2b10af4805a01f17d
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2016-03-31 14:51:57 +0100

    Return anything but unpublished articles through the article link search api

--- app/controllers/links/pages_controller.rb
+++ app/controllers/links/pages_controller.rb
@@ -1,6 +1,6 @@
 class Links::PagesController < PagesController
   def index
-    pages  = Comfy::Cms::Page.includes(:site).published.filter(params.slice(:category))
+    pages  = Comfy::Cms::Page.includes(:site).where('state != ?', 'unpublished').filter(params.slice(:category))
     pages  = Comfy::Cms::Search.new(pages, params[:search]).results if params[:search].present?
 
     @pages = PageMirror.collect(pages)
