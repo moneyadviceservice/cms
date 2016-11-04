959e75cf8392dd6bdfaf6ee4fbe36661a7131799
Jon Gilbraith <jon@hobocode.io>
2016-11-04 08:00:55 +0000

    Update the javascript snippet tool’s table template to include the caption paragraph

--- app/assets/javascripts/comfortable_mexican_sofa/admin/modules/mastalk/snippets-model.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/modules/mastalk/snippets-model.js
@@ -8,7 +8,7 @@ define([], function () {
     collapsible: '$=\r\n# Before you borrow\r\n=$\r\n\r\n$-\r\nFind out if you need to borrow money and whether you can afford it. Learn how to work out the true cost of borrowing.\r\n\r\nTaking control of debt\r\n\r\nWhere to get free debt advice, how to speak to the people you owe money to, and tips to help you pay back your debts in the right order.\r\n-$',
     callout: '$~callout\r\n# Budgeting tips\r\nIn 1985, average first-time buyers needed a deposit of 5% to buy a home - in 2012, this had increased to 20%\r\n*Source: HMS Treasury*\r\n~$\r\n',
     promoBlock: '$bl\r\n\r\n$bl_m\r\n\r\nbl_m$\r\n\r\n$bl_c\r\n\r\nbl_c$\r\n\r\nbl$',
-    table: '| Equity loans | Mortgage guarantees\r\n|---|---\r\n | New-build properties | New-build and pre-owned properties\r\n| Minimum 5% deposit | Minimum 5% deposit',
+    table: '| Equity loans | Mortgage guarantees\r\n|---|---\r\n | New-build properties | New-build and pre-owned properties\r\n| Minimum 5% deposit | Minimum 5% deposit\r\n\r\nA caption for the table\r\n{: .caption}\r\n',
     ticks: '$yes-no\r\n[y] yes [\/y]\r\n[n] no [\/n]\r\n$end',
     videoYoutube: '$~youtube_video\r\nVIDEO_ID\r\nA title for the video\r\n~$',
     videoBrightcove: '$~brightcove_video\r\nVIDEO_ID\r\nA title for the video\r\n~$',
