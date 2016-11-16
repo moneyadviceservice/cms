c29a28076f74722e43a159b981403a7fd4aad728
Jerome Riga <jerome.riga@moneyadviceservice.org.uk>
2016-11-16 11:20:19 +0000

    Changed the way menu is detected to prevent double menu rendering

--- app/models/internal_link.rb
+++ app/models/internal_link.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'nokogiri'
 
 class InternalLink
@@ -7,7 +8,7 @@ class InternalLink
     @source              = source
     @doc                 = Nokogiri::HTML.fragment(source)
     @sections            = doc.css('h2')
-    @internal_links_menu = doc.css('h1 + p + ul')
+    @internal_links_menu = doc.css(existing_menu_selector(@sections[0])) if @sections[0]
     freeze
   end
 
@@ -45,6 +46,16 @@ class InternalLink
   end
 
   def parse_id(s)
-    s.downcase.tr(' ', '-')
+    s.downcase
+      .delete(',?!.:;–')  # deletes punctuation marks and emdash (\u2013)
+      .tr(' ', '-')       # replaces spaces with dash
+  end
+
+  def existing_menu_selector(node)
+    [
+      'a[href="#',
+      parse_id(node.content),
+      '"]'
+    ].join('')
   end
 end
