6d1093632dddf27bb275d1650000b694a36d35cb
Jon Gilbraith <jon@hobocode.io>
2016-11-18 14:12:52 +0000

    Populate some initial clumps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118115306) do
+ActiveRecord::Schema.define(version: 20161118135342) do
 
   create_table "category_promos", force: true do |t|
     t.string  "promo_type"
