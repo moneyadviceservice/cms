f6c1349d475a7a0f874af760350b51bf224d1281
Tomas D'Stefano <tomas_stefano@successoft.com>
2016-11-21 09:27:29 +0000

    Sort by created at on page feedback list

--- app/controllers/page_feedbacks_controller.rb
+++ app/controllers/page_feedbacks_controller.rb
@@ -2,7 +2,7 @@ class PageFeedbacksController < Comfy::Admin::Cms::BaseController
   respond_to :html, :csv
 
   def index
-    @page_feedbacks = PageFeedback.includes(:page).page(params[:page])
+    @page_feedbacks = PageFeedback.includes(:page).page(params[:page]).order('created_at DESC')
     respond_with(@page_feedbacks)
   end
 end
