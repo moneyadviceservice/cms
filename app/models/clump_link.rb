3649f4a2c87b7ded3652172d4a3e40e6dc0d268c
Jon Gilbraith <jon@hobocode.io>
2016-12-08 16:16:26 +0000

    Add validation to ClumpLink
    Validate presence of link fields, but allow all to be blank

--- app/models/clump_link.rb
+++ app/models/clump_link.rb
@@ -2,4 +2,14 @@ class ClumpLink < ActiveRecord::Base
 
   belongs_to :clump
 
+  validates :text_en, presence: true, unless: :empty?
+  validates :text_cy, presence: true, unless: :empty?
+  validates :url_en, presence: true, unless: :empty?
+  validates :url_cy, presence: true, unless: :empty?
+  validates :style, presence: true, unless: :empty?
+
+  def empty?
+    [text_en, text_cy, url_en, url_cy, style].all?(&:blank?)
+  end
+
 end
