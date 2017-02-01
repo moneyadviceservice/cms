3a3b07eb09e97748b09928aa021a91462fd014e2
Jon Gilbraith <jon@hobocode.io>
2017-02-01 14:01:41 +0000

    Bring all clump, clumping and clump link creation into single migration
    Contains approved and properly translated copy

--- db/migrate/20170110153128_create_extra_clump_links.rb
+++
@@ -1,9 +0,0 @@
-class CreateExtraClumpLinks < ActiveRecord::Migration
-  def change
-    Clump.all.each do |clump|
-      (4 - clump.clump_links.count).times do
-        clump.clump_links.create!
-      end
-    end
-  end
-end
