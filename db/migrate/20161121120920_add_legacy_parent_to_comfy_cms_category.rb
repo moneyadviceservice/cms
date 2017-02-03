e84d2af406ec22308e4c0fc996665e324a87a88f
Jon Gilbraith <jon@hobocode.io>
2017-02-03 14:50:57 +0000

    Add category legacy_parent_id column after the parent_id

--- db/migrate/20161121120920_add_legacy_parent_to_comfy_cms_category.rb
+++ db/migrate/20161121120920_add_legacy_parent_to_comfy_cms_category.rb
@@ -1,5 +1,5 @@
 class AddLegacyParentToComfyCmsCategory < ActiveRecord::Migration
   def change
-    add_column :comfy_cms_categories, :legacy_parent_id, :integer
+    add_column :comfy_cms_categories, :legacy_parent_id, :integer, after: :parent_id
   end
 end
