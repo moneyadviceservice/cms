007916db3605b518d1805cf3b503722739abf564
Jon Gilbraith <jon@hobocode.io>
2017-02-09 11:29:55 +0000

    Remove the “embedded google analytics” functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,4 @@ require(['componentLoader', 'jquery'], function(componentLoader, $) {
   'use strict';
 
   componentLoader.init($('body'));
-
-  $('.analytics-button').on('click', function() {
-    $('.analytics-charts').toggle(function() {
-      $('html, body').animate({ scrollTop: 0 }, 500);
-    });
-  });
 });
