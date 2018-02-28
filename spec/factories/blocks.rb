e44359c32c849309e2cdceb4f81cf95f77008be0
Margo Urey <margo@margonline.co.uk>
2018-02-28 10:56:18 +0000

    Use `with_content_like scope` from the Comfortable Mexican Sofa gem

--- spec/factories/blocks.rb
+++ spec/factories/blocks.rb
@@ -1,7 +1,19 @@
 FactoryGirl.define do
   factory :block, class: Comfy::Cms::Block do
     identifier { 'content' }
-    content { 'some block content' }
+    content { 'financial' }
     association :blockable, factory: :page
+
+    trait :debt_content do
+      content { 'debt' }
+    end
+
+    trait :pension_content do
+      content { 'pension' }
+    end
+
+    trait :financial_wellbeing_content do
+      content { 'Financial well being: the employee view' }
+    end
   end
 end
