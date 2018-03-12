0987d0727f611d28591b7c810de54710096b5b12
Margo Urey <margo@margonline.co.uk>
2018-03-12 17:50:23 +0000

    filter documents by filter_type

--- app/controllers/api/documents_controller.rb
+++ app/controllers/api/documents_controller.rb
@@ -6,9 +6,13 @@ module API
     param :locale, String, required: true
     param :page_type, String, required: false
     param :keyword, String, required: false
+    param :blocks, Array, required: false
     def index
       documents = DocumentProvider.new(
-        current_site, params[:document_type], params[:keyword]
+        current_site, 
+        params[:document_type], 
+        params[:keyword], 
+        params[:blocks]
       ).retrieve
 
       render json: documents, meta: { results: documents.count }, root: 'documents'
