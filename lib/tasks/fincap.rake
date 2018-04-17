3ad940294cec44b69bddda2aabb9612cfe76aebb
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-04-17 18:23:39 +0100

    Add simple component to the CMS
    This simple component will accept any content basically
    and the client will decide how to render those components.
    The reason about the name "simple_" is because of the client
    will decide how to render the components and not CMS itself.
    
    Also this commit address the fixes for carriage returns on textarea tags

--- lib/tasks/fincap.rake
+++ lib/tasks/fincap.rake
@@ -32,5 +32,18 @@ namespace :fincap do
         CONTENT
       )
     end
+
+    english_site.layouts.find_or_create_by(
+      identifier: 'article',
+      label: 'Article',
+      content:  <<-CONTENT
+        {{ cms:page:content:rich_text }}
+        {{ cms:page:hero_image:simple_component/https://moneyadviceservice.org.uk/image.jpg }}
+        {{ cms:page:hero_description:simple_component/Description }}
+        {{ cms:page:cta_links:simple_component/[Text Link](https://moneyadviceservice.org.uk/link) }}
+        {{ cms:page:download:simple_component/[Text Link](https://moneyadviceservice.org.uk/link) }}
+        {{ cms:page:feedback:simple_component/email@moneyadviceservice.org.uk.org.uk) }}
+      CONTENT
+    )
   end
 end
---
+++ spec/lib/comfortable_mexican_sofa/extensions/tags/simple_component_spec.rb
@@ -0,0 +1,36 @@
+describe ComfortableMexicanSofa::Tag::SimpleComponent do
+  describe '.regex_tag_signature' do
+    subject { tag_signature.match(described_class.regex_tag_signature('test')) }
+
+    context 'when a simple component tag' do
+      let (:tag_signature) { '{{ cms:page:test:simple_component/param1, param2 }}' }
+
+      it 'matches' do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context 'when not an simple component tag' do
+      let (:tag_signature) { '{{ cms:page:test:string }}' }
+
+      it 'does not match' do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe '#identifier' do
+    let (:tag) { described_class.initialize_tag(nil, tag_signature) }
+    subject { tag.identifier }
+
+    context 'when valid arguments' do
+      let (:tag_signature) do
+        '{{ cms:page:hero_image:simple_component//image.jpg }}'
+      end
+
+      it 'returns array of params' do
+        expect(subject).to eq('component_hero_image')
+      end
+    end
+  end
+end
