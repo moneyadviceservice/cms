4f86a5501bf347393676de348a7ca00d29468521
osharpe <oliver.sharpe@moneyadviceservice.org.uk>
2018-04-18 14:43:42 +0100

    Update db yaml and env var

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_controller.asset_host = ENV.fetch('MAS_CMS_URL', 'http://localhost:3000')
+  config.action_controller.asset_host = ENV.fetch('FINCAP_MAS_CMS_URL', 'http://localhost:3000')
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
