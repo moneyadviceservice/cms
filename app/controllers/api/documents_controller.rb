721f131625d5ad149dc1adad10325663b04e7621
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-05-15 17:08:09 +0100

    Add pagination to API /documents
    You can ask different pages by passing on the request

--- app/controllers/api/documents_controller.rb
+++ app/controllers/api/documents_controller.rb
@@ -1,5 +1,8 @@
 module API
   class DocumentsController < APIController
+    PAGE_NUMBER = 1
+    PER_PAGE = 20
+
     before_action :find_site
 
     api :GET, '/:locale/documents'
@@ -8,18 +11,43 @@ module API
     param :keyword, String, required: false
     param :blocks, Array, required: false
     def index
-      documents = DocumentProvider.new(
-        current_site, 
-        params[:document_type], 
-        params[:keyword], 
-        params[:blocks]
-      ).retrieve
-
       if documents
-        render json: documents, meta: { results: documents.size }, root: 'documents'
+        render json: paginated_documents, meta: meta_data, root: 'documents'
       else
         render json: { message: 'Bad request' }, status: 400
       end
+    end
+
+    private
+
+    def documents
+      @documents ||= DocumentProvider.new(
+        current_site,
+        params[:document_type],
+        params[:keyword],
+        params[:blocks]
+      ).retrieve
+    end
+
+    def paginated_documents
+      documents.page(page).per(per_page)
+    end
+
+    def meta_data
+      {
+        results: documents.size,
+        page: page,
+        per_page: per_page,
+        total_pages: paginated_documents.total_pages
+      }
+    end
+
+    def page
+      Integer(params[:page] || PAGE_NUMBER)
+    end
+
+    def per_page
+      Integer(params[:per_page] || PER_PAGE)
     end
   end
 end
