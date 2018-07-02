984f01efcae9a4071115a12d8a04a9ae3f786664
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-07-02 13:28:16 +0100

    Add the structure to convert prismic documents into html and markdown
    I decided to use Nokogiri for find the content in the text and replace
    them with the actual html tags.
    This works for paragraphs, bold and heading tags.
    Also I added real examples from Prismic on the test cases.

---
+++ lib/prismic/tag_converter.rb
@@ -0,0 +1,77 @@
+module Prismic
+  class TagConverter
+    PRISMIC_FIELD = {
+      paragraph: '<p/>',
+      heading1: '<h1/>',
+      heading2: '<h2/>',
+      heading3: '<h3/>',
+      heading4: '<h4/>',
+      heading5: '<h5/>',
+      heading6: '<h6/>',
+      :'list-item' => '<li/>',
+      embed: '',
+      image: '',
+      :'o-list-item' => '<li/>'
+    }.stringify_keys.freeze
+
+    PRISMIC_FORMAT = {
+      strong: 'strong'
+    }.stringify_keys.freeze
+    attr_accessor :document_fragment, :fragment_type, :text, :text_format
+    include ActiveModel::Model
+
+    # Transform Prismic fields into html tags. The possible values:
+    #  [
+    #   "paragraph",
+    #   "list-item",
+    #   "embed",
+    #   "image",
+    #   "o-list-item",
+    #   "heading2",
+    #   "heading3",
+    #   "heading4",
+    #   "heading1",
+    #   "heading6",
+    #   "heading5"
+    #  ]
+    #
+    def to_html
+      html_tag = PRISMIC_FIELD[fragment_type].to_s
+
+      fragment_text.wrap(html_tag) if html_tag
+
+      convert_special_formats(
+        text: text,
+        text_format: text_format,
+        document_fragment: document_fragment
+      )
+
+      document_fragment.to_html
+    end
+
+    private
+
+    def fragment_text
+      document_fragment.xpath('.//text()')
+    end
+
+    # Prismic formats. The "spans" node has this possible values:
+    #
+    #  ["strong", "hyperlink", "em", "label"]
+    #
+    def convert_special_formats(text:, text_format:, document_fragment:)
+      text_format.each do |format|
+        start_at = format['start']
+        end_at = format['end']
+        html_tag_format = PRISMIC_FORMAT[format['type']]
+        content = text[start_at, end_at]
+
+        if html_tag_format
+          fragment_text.first.replace(
+            text.gsub(content, "<#{html_tag_format}>#{content}</#{html_tag_format}>")
+          )
+        end
+      end
+    end
+  end
+end
