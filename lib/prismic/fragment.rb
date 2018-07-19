ec98cac32dc307e37632e8aca9edd7322951e3e3
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-07-19 11:16:48 +0100

    Add embed videos conversion
    I also took the opportunity to fix the rubocop offenses on the spec file

--- lib/prismic/fragment.rb
+++ lib/prismic/fragment.rb
@@ -1,8 +1,9 @@
 module Prismic
   class Fragment
-    attr_reader :field_type, :text, :text_format
+    attr_reader :original_fragment, :field_type, :text, :text_format
 
     def initialize(fragment)
+      @original_fragment = fragment
       @field_type = fragment['type']
       @text = fragment['content']['text'] if fragment['content']
       @text_format = fragment['content']['spans'] if fragment['content']
