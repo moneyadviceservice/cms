3efeb1152f3a3d9b5263f62f123e744e69faa2b7
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-07-19 12:43:49 +0100

    Convert html attributes to markdown for migrate prismic documents

--- lib/prismic/cms_converter.rb
+++ lib/prismic/cms_converter.rb
@@ -13,7 +13,9 @@ module Prismic
     def convert
       document.row.each do |attribute, content|
         if content.is_a?(Array) && content.first.is_a?(Hash)
-          converted_data[attribute] = HTMLConverter.new(content).to_html
+          html = HTMLConverter.new(content).to_html
+          converted_data[attribute] = html
+          converted_data["#{attribute}_markdown"] = ReverseMarkdown.convert(html)
         else
           converted_data[attribute] = content
         end
