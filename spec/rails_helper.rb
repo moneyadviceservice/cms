3536d6860cf5fd832cb293c83ad063a350628b71
Giuseppe Lobraico <Giuseppe.Lobraico@MoneyAdviceService.org.uk>
2018-08-13 09:25:12 +0100

    Remove globally available spec helper
    The `with_versioning` helper is available to the whole app when running in test mode,
    while it should be loaded using the rspec helper module that `paper_trail` itself provides.
    
    Plase look at: https://github.com/paper-trail-gem/paper_trail/blob/3.0-stable/lib/paper_trail/frameworks/rspec.rb
    and: https://github.com/paper-trail-gem/paper_trail/blob/3.0-stable/lib/paper_trail/frameworks/rspec/helpers.rb#L6-L12
    for more info.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,6 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
 
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
