0a61146a7461ced7042cc36780a2ebf3f4d156fd
Marc Sardon <pringaohowto@gmail.com>
2018-10-15 18:34:42 +0100

    Configuration changes after rails update rake task
    Rake task for rails upgrade brought some config changes.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module Cms
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     config.autoload_paths += %W(#{config.root}/lib)
 
     config.assets.precompile += %w(comfortable_mexican_sofa/admin/basic.css rsvp/rsvp.js)
