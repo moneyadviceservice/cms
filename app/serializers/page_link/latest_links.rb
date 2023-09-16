1064e6b3ca5196f5a15eff782022c68bfb01aeaa
Ben Lovell <benjamin.lovell@gmail.com>
2023-09-16 11:24:51 +0100

    Attempt to resolve the autoloading issues on boot

--- app/serializers/page_link/latest_links.rb
+++ app/serializers/page_link/latest_links.rb
@@ -1,8 +1,10 @@
+require Rails.root.join('lib', 'publify', 'api')
+
 module PageLink
   class LatestLinks < PageLink::Base
     def as_json
       Rails.cache.fetch('publify_api_latest_links_3', expires_in: 5.minutes) do
-        Publify::API.latest_links(3).map do |input_blog_post|
+        ::Publify::API.latest_links(3).map do |input_blog_post|
           {
             title: input_blog_post['title'],
             path: input_blog_post['link']
